3f95665c65f04621ed2dd3a8d95756e62f4e9b3f
AliNisarAhmed <ali.nisar87@gmail.com>
2019-04-15 20:38:41 -0600

    solved linear search

--- colt/recursive2.js
+++ colt/recursive2.js
@@ -62,3 +62,147 @@ function capitalizeFirst([first, ...rest]) {
 }
 
 console.log(capitalizeFirst(['car','taco','banana'])); // ['Car','Taco','Banana'])
+
+
+//===============================
+
+function nestedEvenSum(obj) {
+  let sum = 0;
+  for (let [key, value] of Object.entries(obj)) {
+    if (typeof value === "number" && value % 2 === 0) {
+      sum += value;
+    } else if (typeof value === "object") {
+      sum += nestedEvenSum(value);
+    }
+  }
+  return sum;
+}
+
+
+var obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup"
+    }
+  }
+}
+
+var obj2 = {
+  a: 2,
+  b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+  c: {c: {c: 2}, cc: 'ball', ccc: 5},
+  d: 1,
+  e: {e: {e: 2}, ee: 'car'}
+};
+
+console.log(nestedEvenSum(obj1)); // 6
+console.log(nestedEvenSum(obj2)); // 10
+
+//===============================================
+
+
+function capitalizedWords([first, ...rest]) {
+
+  if (first) {
+    return [capitalizeWord(first), ...capitalizedWords(rest)];
+  } else {
+    return '';
+  }
+
+  function capitalizeWord(str) {
+    return [...str].reduce((acc, x) => [...acc, x.toUpperCase()], []).join('');
+  }
+}
+
+let words = ['i', 'am', 'learning', 'recursion'];
+console.log(capitalizedWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
+
+
+//=======================================================
+
+function stringifyNumbers(obj) {
+  let newObj = {};
+  for (let [key, value] of Object.entries(obj)) {
+    if (typeof value === "object") {
+      if (Array.isArray(value) || value === null || typeof value === "function") {
+        newObj[key] = value;
+      } else {
+        newObj[key] = stringifyNumbers(value);
+      }
+    } else if (typeof value === "number") {
+      newObj[key] = String(value);
+    } else {
+      newObj[key] = value;
+    }
+  }
+  return newObj;
+}
+
+
+let obj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+
+
+console.log(stringifyNumbers(obj));
+
+/*
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+*/
+
+
+//====================================================
+
+function collectStrings(obj) {
+  let collection = [];
+  for (let [key, val] of Object.entries(obj)) {
+    if (typeof val === "string") {
+      collection.push(val);
+    } else if (typeof val === "object") {
+      if (!Array.isArray(val) && val != null && typeof val !== "function") {
+        collection = [...collection, ...collectStrings(val)];
+      }
+    }
+  }
+  return collection;
+}
+
+obj = {
+  stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+console.log(collectStrings(obj)) // ["foo", "bar", "baz"])
