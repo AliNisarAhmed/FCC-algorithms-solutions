22c6d13b3afdd3057143c5c3e79a8333a5e408c4
AliNisarAhmed <ali.nisar87@gmail.com>
2019-04-16 23:58:58 -0600

    solving binary search

---
+++ colt/binarySearch.js
@@ -0,0 +1,20 @@
+function binarySearch(arr, val) {
+  let left = 0, right = arr.length - 1;
+  let mid;
+  while (left < right) {
+    mid = Math.ceil((left + right) / 2);
+    if (arr[mid] === val) {
+      return mid;
+    } else if (val > arr[mid]) {
+      left = mid - 1;
+    } else {
+      right = mid + 1;
+    }
+  }
+  return -1;
+} 
+
+
+
+console.log(binarySearch([1, 2, 3, 4, 5], 1)); // 2
+// binarySearch([1, 2, 3, 4, 5], 6); // -1
