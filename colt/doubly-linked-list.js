ed5039785ad5658471f1ed7dae4d80c4ad66dcec
AliNisarAhmed <ali.nisar87@gmail.com>
2019-05-05 18:55:29 -0600

    doubly linked list insert done

--- colt/doubly-linked-list.js
+++ colt/doubly-linked-list.js
@@ -40,13 +40,86 @@ class DoublyLinkedList {
     this.length--;
     return nodeToBeRemoved;
   }
+
+  shift() {
+    if (this.length === 0) return undefined;
+    let currentHead = this.head; 
+    if (this.length === 1) {
+      currentHead.next = null;
+      currentHead.prev = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      currentHead.next = null;
+    }
+    this.length--;
+    return currentHead;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let mid = Math.floor(this.length / 2);
+    if (index <= mid) {
+      let node = this.head;
+      let count = 0;
+      while (count !== index) {
+        node = node.next;
+        count++;
+      }
+      return node;
+    } else {
+      let node = this.tail;
+      let count = this.length - 1;
+      while (count !== index) {
+        node = node.prev;
+        count--;
+      }
+      return node;
+    }
+  }
+
+  set(index, value) {
+    let node = this.get(index);
+    if (!node) return false;
+    node.val = value;
+    return true;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(value);
+    if (index === this.length) return !!this.push(value);
+    let node = this.get(index - 1);
+    let newNode = new Node(value);
+    let nextNode = node.next;
+    newNode.next = nextNode;
+    nextNode.prev = newNode;
+    newNode.prev = node;
+    node.next = newNode;
+    this.length++;
+    return true;
+  }
 }
 
 let list = new DoublyLinkedList();
-list.push(1).push(2).push(3);
+list.push(1).push(2).push(3).push(4).push(5).push(6);
 // console.log(list.pop());
-console.log(list.pop());
-console.log(list.pop());
+console.log(list.insert(3, 200));
+// console.log(list.pop());
 // list.pop();
 // list.pop();
-// console.log(list)
+console.log(list);
